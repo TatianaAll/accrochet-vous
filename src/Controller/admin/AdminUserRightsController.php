ca8c005539783a32da004c2c1dbdf4d5326c1cd1
Tatiana <tatiana.allery@orange.fr>
2025-01-06 16:15:43 +0100

    feat: list user and give redactor status

---
+++ src/Controller/admin/AdminUserRightsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class AdminUserRightsController extends AbstractController
+{
+    #[Route(path: '/admin/rights/list_users', name: 'admin_users_list', methods: ['GET'])]
+    #[IsGranted(new Expression('is_granted("ROLE_SUPER_ADMIN") or is_granted("ROLE_ADMIN")'))]
+    public function listAdmin(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('admin/rights/list_users.html.twig', ['users' => $users]);
+    }
+
+    #[Route(path: '/admin/rights/{id}/redactor_status', name: 'admin_users_redactor', requirements: ["id"=>"\d+"], methods: ['GET'])]
+    #[IsGranted(new Expression('is_granted("ROLE_SUPER_ADMIN") or is_granted("ROLE_ADMIN")'))]
+    public function giveRedactorStatus(int $id, UserRepository $userRepository, EntityManagerInterface $entityManager) : Response
+    {
+        $user = $userRepository->find($id);
+        if(!$user){
+            $this->addFlash("error", "Utilisateur non trouvé");
+            return $this->redirectToRoute("admin_users_list");
+        }
+
+        $user->setRoles(["ROLE_USER", "ROLE_REDACTOR"]);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        $this->addFlash("success", "Rôle rédacteur attribué à" . $user->getUsername() . " !");
+        return $this->redirectToRoute('admin_users_list');
+    }
+
+}
