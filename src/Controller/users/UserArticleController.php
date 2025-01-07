31e8c7d0cbc9c08d39cfd3cfe02753fc285b6ee3
Tatiana <tatiana.allery@orange.fr>
2025-01-07 11:46:56 +0100

    feat: add mail send after article submission

--- src/Controller/users/UserArticleController.php
+++ src/Controller/users/UserArticleController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -18,7 +20,7 @@ class UserArticleController extends AbstractController
     #[Route(path: '/user/article/create', name: 'user_article_create', methods: ['POST', 'GET'])]
     #[IsGranted(new Expression('is_granted("ROLE_REDACTOR")'))]
     public function userSubmitArticle(Request $request, ImageImporter $imageImporter,
-                                      EntityManagerInterface $entityManager) :Response
+                                      EntityManagerInterface $entityManager, MailerInterface $mailer) :Response
     {
         //new instance of article
         $userArticle = new Article();
@@ -45,6 +47,20 @@ class UserArticleController extends AbstractController
 
                 $entityManager->persist($userArticle);
                 $entityManager->flush();
+
+                $email = new Email();
+
+                //mail template
+                //give the template the view associate and the values
+                $emailTemplate = $this->renderView('mails/article_submission.html.twig', ['article' => $userArticle]);
+
+                //j'envoie avec mailer
+                $mailer->send(
+                    $email->from('noreply@accrochetvous.com')
+                        ->to('contact@accrochetvous.com')
+                        ->subject('Nouvel article soumis')
+                        ->html($emailTemplate)
+                );
 
                 $this->addFlash("success", "Modèle envoyé aux administrateurs pour relecture ;)");
                 return $this->redirectToRoute('user_current_profile');
