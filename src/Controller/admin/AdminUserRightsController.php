e5513796cf2cbf774d7468de8f774bc5d85e902c
Tatiana <tatiana.allery@orange.fr>
2025-01-07 16:12:26 +0100

    feat: mail when admin give redactor right

--- src/Controller/admin/AdminUserRightsController.php
+++ src/Controller/admin/AdminUserRightsController.php
@@ -7,6 +7,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -22,7 +24,8 @@ class AdminUserRightsController extends AbstractController
 
     #[Route(path: '/admin/rights/{id}/redactor_status', name: 'admin_users_redactor', requirements: ["id"=>"\d+"], methods: ['GET'])]
     #[IsGranted(new Expression('is_granted("ROLE_SUPER_ADMIN") or is_granted("ROLE_ADMIN")'))]
-    public function giveRedactorStatus(int $id, UserRepository $userRepository, EntityManagerInterface $entityManager) : Response
+    public function giveRedactorStatus(int $id, UserRepository $userRepository,
+                                       EntityManagerInterface $entityManager, MailerInterface $mailer) : Response
     {
         $user = $userRepository->find($id);
         if(!$user){
@@ -34,7 +37,18 @@ class AdminUserRightsController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
 
-        $this->addFlash("success", "Rôle rédacteur attribué à" . $user->getUsername() . " !");
+        $email = new Email();
+
+        $emailTemplate = $this->renderView('mails/giving_right.html.twig', ['user' => $user]);
+
+        $mailer->send(
+            $email->from('noreply@accrochetvous.com')
+                ->to($user->getEmail())
+                ->subject('Droits rédacteur attribués')
+                ->html($emailTemplate)
+        );
+
+        $this->addFlash("success", "Rôle rédacteur attribué à " . $user->getUsername() . " !");
         return $this->redirectToRoute('admin_users_list');
     }
 
