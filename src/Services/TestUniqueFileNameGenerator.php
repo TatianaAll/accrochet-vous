defd9baf7af204d55a3e3c3e76435e832bc2eb01
Tatiana <tatiana.allery@orange.fr>
2025-01-08 12:29:48 +0100

    feat: UT for UniqueFileGenerator

---
+++ src/Services/TestUniqueFileNameGenerator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use PHPUnit\Framework\TestCase;
+
+
+class TestUniqueFileNameGenerator extends TestCase
+{
+    public function  testGenerateUniqueFilename()
+    {
+        //Instance the class to test
+        $uniqueFileNameGenerator = new UniqueFileNameGenerator();
+
+        // create a variable with the new instance to test and call it with the method
+        $uniqueFilename = $uniqueFileNameGenerator->generateUniqueFileName('hello', 'jpeg');
+
+        //test conditions given to the unique file name generate previously
+        // php bin/phpunit src/Services/TestUniqueFilenameGenerator.php
+        $this->assertStringContainsString('jpeg', $uniqueFilename);
+        $this->assertStringContainsString(time(), $uniqueFilename);
+    }
+}
